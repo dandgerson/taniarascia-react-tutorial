a80f515e2ff7acb0f6e6af6f351351fe2615d823
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-08 00:57:50 +0300

    add Form.js & update state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+import Table from './Table';
+import Form from './Form';
 export default class App extends Component {
+  state = {
+    characters: [],
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    })
+  }
+
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  }
+
   render() {
+    const { characters } = this.state;
+
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
---
+++ src/Form.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class From extends Component {
+  constructor(props) {
+    super(props)
+
+    this.initialState = {
+      name: '',
+      job: '',
+    }
+
+    this.state = this.initialState;
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  submitForm = () => {
+    this.props.handleSubmit(this.state);
+    this.setState(this.initialState);
+  }
+
+  render() {
+    const { name, job } = this.state;
+
+    return (
+      <form>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={this.handleChange} />
+        <input
+          type="text"
+          name="job"
+          value={job}
+          onChange={this.handleChange} />
+        <input
+          type="button"
+          value="Submit"
+          onClick={this.submitForm} />
+      </form>
+    )
+  }
+}
