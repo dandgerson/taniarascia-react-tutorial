56d46d0ae9833c375be14f88bff2a96f7c1f9b0c
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-08 17:50:27 +0300

    Updates

---
+++ precache-manifest.1dc408cdfdf7fdbdb961e5c37068e81e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94162148a907e16e9f7201ee9e8a5fd9",
+    "url": "/taniarascia-react-tutorial/index.html"
+  },
+  {
+    "revision": "93da79726e7aa07ff2fc",
+    "url": "/taniarascia-react-tutorial/static/css/main.545ae54c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/taniarascia-react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "93da79726e7aa07ff2fc",
+    "url": "/taniarascia-react-tutorial/static/js/main.bcc5690e.chunk.js"
+  },
+  {
+    "revision": "0c7aa3a93ef8ad3c95aa",
+    "url": "/taniarascia-react-tutorial/static/js/runtime~main.6aecaade.js"
+  }
+]);
